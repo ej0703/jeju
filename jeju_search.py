08a8698cdf42936cd8b02609dd5c8080c96e5f86
Eunji Lee <ej_0703@naver.com>
2020-03-02 23:01:15 +0900

    200302

---
+++ jeju_search.py
@@ -0,0 +1,23 @@
+import pymysql
+
+#MySQL이랑 연결 한 거, db 저장한 거에 따라 내용 변경해서 사용할 것
+conn = pymysql.connect(host='localhost', user='root', password='1234', db='jeju_info', charset='utf8')
+
+# Connection 으로부터 Dictoionary Cursor 생성
+curs = conn.cursor(pymysql.cursors.DictCursor)
+
+#버튼 이벤트로 받은 단어를 받을 변수(임시로 그냥 프롬프트에서 입력받는 것으로 했음)
+search=input()
+
+#SQL문으로 검색
+sql = "select category, loc_name, tel, address from jeju where category=%s or loc_name=%s"
+curs.execute(sql, (search, search))
+
+# 데이타 Fetch
+rows = curs.fetchall()
+for row in rows:
+    print(row)
+    #print(row['category'], row['loc_name'], row['tel'], row['address'])
+ 
+# Connection 닫기
+conn.close()
