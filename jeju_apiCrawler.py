83652e92e5db121331c2f09dbf7d217384ab5768
Eunji Lee <ej_0703@naver.com>
2020-03-06 15:16:58 +0900

    crawling_update

--- jeju_apiCrawler.py
+++ jeju_apiCrawler.py
@@ -3,17 +3,28 @@ import xmltodict
 import json
 
 # 공공데이터포털 사이트로 크롤링 요청 위한 인증키와 요청 메세지
-# 우선은 페이지 수와 출력 줄 수 설정 안 해둠
-key = "wGCosOLwUd0xyA6jmnIMi4dbtYCJYKBl6Af8HjuCMhPGi9tprfPpeG9FjrJWOdopp2pfZgg4ChyhAb%2FRgMvIRg%3D%3D"
-url = "http://openapi.jejusi.go.kr/rest/PublicBikeInfoService/getPublicBikeInfoList?ServiceKey={}".format(key)
+key = "y3hJyZd4gVr4xFzmKzqb53nioC68jdCSKnf8lZYuhvp9VOgSJ5N1GgtPP7fEVhcasTygTCSvigiqKj2syoMEyg%3D%3D"
+url_cycle="http://openapi.jejusi.go.kr/rest/PublicBikeInfoService/getPublicBikeInfoList?ServiceKey={}&pageNo=1&numOfRows=10".format(key)
+url_parking = "http://openapi.jejuits.go.kr/OPEN_API/pisInfo/getPisInfo?serviceKey={}&pageNo=1&numOfRows=100".format(key)
 
 # 웹에 요청
-content = requests.get(url).content
+content_cycle = requests.get(url_cycle).content
+content_parking = requests.get(url_parking).content
 # xml형식은 정보 분리하기 힘들어서 json으로 파싱
-dict = xmltodict.parse(content)
-# json으로 변화된 정보를 구분해서 출력
-jsonString = json.dumps(dict['rfcOpenApi']['body'], ensure_ascii = False)
-jsonObj = json.loads(jsonString)
+dict_cycle = xmltodict.parse(content_cycle)
+dict_parking = xmltodict.parse(content_parking)
+# json으로 변화된 정보를 구분해서 출력-공공자전거
+jsonString_cycle = json.dumps(dict_cycle['rfcOpenApi']['body'], ensure_ascii = False)
+jsonObj_cycle = json.loads(jsonString_cycle)
 
-for list in jsonObj['data']['list']:
+for list in jsonObj_cycle['data']['list']:
     print(list['address'],list['location'],list['retal_enable_num'])
+
+print('\n')
+
+# json으로 변화된 정보를 구분해서 출력-주차장
+jsonString_parking = json.dumps(dict_parking['ServiceResult']['msgBody'], ensure_ascii = False)
+jsonObj_parking = json.loads(jsonString_parking)
+
+for list in jsonObj_parking['itemList']:
+    print(list['ISTL_LCTN_ADDR'],list['WHOL_NPLS'],list['GNRL_RMND_PRZN_NUM'])
