abc45de9b2db7315a321ac442376a1b8f0a374e8
Eunji Lee <ej_0703@naver.com>
2020-03-10 15:51:56 +0900

    DB + csv save

--- jeju_search.py
+++ jeju_search.py
@@ -1,7 +1,8 @@
 import pymysql
+import pandas as pd
 
 #DB 연동 -MySQL
-conn = pymysql.connect(host='192.168.0.22', user='root', password='1234', db='jeju_info', charset='utf8')
+conn = pymysql.connect(host='192.168.0.52', user='root', password='1234', db='jeju_info', charset='utf8')
 
 #Connection 으로부터 Dictoionary Cursor 생성
 curs = conn.cursor(pymysql.cursors.DictCursor)
@@ -10,7 +11,7 @@ curs = conn.cursor(pymysql.cursors.DictCursor)
 search=input()
 
 #SQL문으로 검색(필요하다고 생각한 정보만 검색했음)
-sql = "select category, loc_name, tel, address from jeju where category like '%"+search+"%'"+" or loc_name like '%"+search+"%';"
+sql = "select x, y, category, loc_name, tel, address from jeju where category like '%"+search+"%'"+" or loc_name like '%"+search+"%';"
 curs.execute(sql)
 
 #데이타 Fetch
@@ -18,6 +19,9 @@ rows = curs.fetchall()
 for row in rows:
     #print(row) #해당 로우들 전체 출력
     print(row['category'], row['loc_name'], row['tel'], row['address'])
+    # 검색결과 csv 파일 저장
+    dataframe = pd.DataFrame(rows)
+    dataframe.to_csv("db.csv",header=False, index=False, encoding ='euc-kr')
  
 #Connection 닫기
 conn.close()
